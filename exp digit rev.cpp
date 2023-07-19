ed6ee955371cf96a1619e84095733df47a110c63
mallicktariq46 <139535305+mallicktariq46@users.noreply.github.com>
2023-07-19 10:18:07 +0530

    Add files via upload

---
+++ exp digit rev.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int>v;
+    //int i=0; 
+    while(n>9){
+        v.push_back(n%10);
+        n/=10;
+       // i++;
+    }
+    cout<<n<<endl;
+    vector<int>::iterator it;
+    it=v.end();
+    it--;
+
+    while(it!=v.begin()){
+         cout<<*it<<endl;
+        it--;
+}
+    cout<<*(v.begin());
+    return 0;
+}
+/*/
+name: tariq akhlak
+prn : 22070123124
+exp : print digits.
+n0 = 54321
+5
+4
+3
+2
+1
