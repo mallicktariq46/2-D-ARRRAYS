dc41cd7954eb7d2b8792c933b2fcac0966179840
mallicktariq46 <139535305+mallicktariq46@users.noreply.github.com>
2023-10-04 09:17:24 +0530

    Update exp 1.cpp

--- exp 1.cpp
+++ exp 1.cpp
@@ -1,12 +1,1 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    cout<<"HELLO WORLD"<<endl;
-    return 0;
-}
-/*/
-    name : atriq akhlak.
-    prn : 22070123124
-    exp : print "hello world",
-    output : hello world.
-    /*/
+
